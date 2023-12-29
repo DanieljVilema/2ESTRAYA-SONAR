200edd7f9a696e03fefda9681b5d0e964070e5ce
Dalay20 <dalay@espol.edu.ec>
2023-12-29 00:15:06 -0500

    nuevo

--- Juego1/src/main/java/ec/edu/espol/juego1/PruebaController.java
+++ Juego1/src/main/java/ec/edu/espol/juego1/PruebaController.java
@@ -2,18 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package Control;
+package ec.edu.espol.juego1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
  * @author naomi
  */
-public class TablaController implements Initializable {
+public class PruebaController implements Initializable {
+
+    @FXML
+    private Button b1;
 
     /**
      * Initializes the controller class.
--- Juego1/src/main/java/ec/edu/espol/juego1/TablaController.java
+++ Juego1/src/main/java/ec/edu/espol/juego1/TablaController.java
@@ -1,12 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package ec.edu.espol.juego1;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+/**
+ * FXML Controller class
+ *
+ * @author naomi
+ */
+public class TablaController implements Initializable {
 
-public class PrimaryController {
 
     @FXML
     private AnchorPane lblPerdidos;
@@ -58,8 +70,14 @@ public class PrimaryController {
     private Button mnuEstadisticas;
     @FXML
     private Label Fondo;
-
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    @FXML
+    private Label blPerdidos;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
 }
